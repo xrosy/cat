cce5df0654c26ed453f2271042ad5255139e9494
za-chenzhenyuan <jason@chenzhenyuan.com>
2019-11-27 16:42:17 +0800

    update version code

--- release/libs/emoji.js
+++ release/libs/emoji.js
@@ -1,2 +1,2 @@
-"use strict";Object.defineProperty(exports,"__esModule",{value:!0}),exports["default"]=void 0;var _default={check:"\u2714",cross:"\u2718",info:"\u2139",excl:"\u203C",rdsh:"\u21B3"};exports["default"]=_default;
+"use strict";Object.defineProperty(exports,"__esModule",{value:!0}),exports["default"]=void 0;var _default={success:"\u2714",check:"\u2713",error:"\u2718",cross:"\u2718",warn:"\u203C",excl:"\u203C",info:"\u2139",debug:"\u2721",rdsh:"\u21B3",log:"\xBB"};exports["default"]=_default;
 //# sourceMappingURL=emoji.js.map
